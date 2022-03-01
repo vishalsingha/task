b4f8266be7faee2ad201d679cf3768472dc67224
VISHAL SINGH <56965195+vishalsingha@users.noreply.github.com>
2022-03-01 23:06:44 +0530

    removed url with re_path

--- project/urls.py
+++ project/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
 
 
 urlpatterns = [
@@ -32,4 +31,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 else: 
     # set it to full path when debug = False
-    urlpatterns += [url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})]
+    urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})]
